4d48947132ba99841eaa02f9bacdb45f6d3e70c8
smarthug <chs1402@nexivil.com>
2020-03-11 20:55:39 +0900

    camera-controls

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,22 +1,78 @@
-import React from "react";
-import DockLayout from "rc-dock";
-import './dock.css'
+import React from 'react';
 
-const defaultLayout = {
+import {DockLayout, DockContextType} from 'rc-dock';
+
+import 'rc-dock/dist/rc-dock.css'
+
+//import './dock.css'
+
+import Scene from './scene'
+
+let tab = {
+  content: <div>Tab Content</div>,
+  closable: true,
+};
+
+let layout = {
     dockbox: {
       mode: 'horizontal',
       children: [
         {
-          tabs: [
-            {id: 'tab1', title: 'tab1', content: <div>Hello World</div>},
-            {id: 'tab2', title: 'tab1', content: <div>Hello World</div>}
+          mode: 'vertical',
+          size: 200,
+          children: [
+            {
+              tabs: [{...tab, id: 't1', title: 'Tab 1'}, {...tab, id: 't2', title: 'Tab 2'}],
+            },
+            {
+              tabs: [{
+                ...tab, id: 't3', title: 'Min Size', content: (
+                  <div>
+                    <p>This tab has a minimal size</p>
+                    150 x 150 px
+                  </div>
+                ), minWidth: 150, minHeight: 150,
+              }, {...tab, id: 't4', title: 'Tab 4'}],
+            },
           ]
-        }
+        },
+        {
+          size: 1000,
+          tabs: [
+            {
+              ...tab, id: 't5', title: 'basic demo', content: (
+                <Scene />
+              ),
+            },
+           
+          ],
+          panelLock: {panelStyle: 'main'},
+        },
+        {
+          size: 200,
+          tabs: [{...tab, id: 't8', title: 'Tab 8'}],
+        },
       ]
-    }
-  };
-
-
-export default function Main() {
-  return <DockLayout style={{height: "100%", width: "100%"}} defaultLayout={defaultLayout} />;
+    },
+    
+  }
+;
+if (window.innerWidth < 600) {
+  // remove a column for mobile
+  layout.dockbox.children.pop();
 }
+
+let count = 0;
+
+export default class Demo extends React.Component {
+
+  
+
+  render() {
+    return (
+      <DockLayout defaultLayout={layout} style={{position: 'absolute', left: 10, top: 10, right: 10, bottom: 10}}/>
+    );
+  }
+}
+
+
--- src/Pages/Main/scene.js
+++ src/Pages/Main/scene.js
@@ -1,32 +1,56 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
+import React, { useRef, useState, useEffect } from 'react'
+import { Canvas, useFrame, useThree } from 'react-three-fiber'
 
+import * as THREE from 'three'
+import CameraControls from 'camera-controls'
 
+CameraControls.install({ THREE: THREE });
+
+const clock = new THREE.Clock();
+
+let cameraControls
 
 function Box(props) {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef()
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  const {
+    gl,                           // WebGL renderer
+    scene,                        // Default scene
+    camera,                       // Default camera
+  } = useThree()
+
+  cameraControls = new CameraControls(camera, gl.domElement);
+
+
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    const delta = clock.getDelta();
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    cameraControls.update(delta)
     
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
-    
-    return (
-      <mesh
-        {...props}
-        ref={mesh}
-        scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-        onClick={e => setActive(!active)}
-        onPointerOver={e => setHover(true)}
-        onPointerOut={e => setHover(false)}>
-        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-        <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    )
-  }
+  })
+
+
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 export default function Main() {
   return (
